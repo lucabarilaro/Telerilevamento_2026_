4ea9d5ab113c28d3e2b3b96ee2889bccff6af575
lucabarilaro <luca.barilaro@gmail.com>
2026-03-23 15:56:25 +0100

    Update 02_visualization.R

--- Code/02_visualization.R
+++ Code/02_visualization.R
@@ -97,18 +97,77 @@ plot(sentinel[[4]])
 plot(sentinel[[2]])
 
 
+## stack
+#metodo per sovrapporre immagini (bande), creando unico file multistrato
+b2 <- im.import("sentinel.dolomites.b2.tif") 
+b3 <- im.import("sentinel.dolomites.b3.tif")
+b4 <- im.import("sentinel.dolomites.b4.tif")
+b8 <- im.import("sentinel.dolomites.b8.tif")
+
+##NOTA BENE: stack funziona solo con immagini con la stessa banda
+# e sistemi di riferimento uguali
 
 
+## ggplot2
+#metodo per confrontare immagini (bande)
 
-#stack prende tutte immagini e le mette insieme
+p1 <- im.ggplot(b8) 
+#funzione im.ggplot() converte un'immagine satellitare (raster) 
+#in formato compatibile con ggplot2, permettendo creazione mappe con scale di colore come viridis
+p2 <- im.ggplot(b4)
+p1 + p2
+
+
+###Ricapitolando, esistono più metodi per Multiframe:
+# 1. par(mfrow=c(1,2))
+# 2. im.multiframe(1,2)
+# 3. stack
+# 4. ggplot2 & patchwork
+
+
+## RGB plotting
+#tecnica che permette di visualizzare un'immagine satellitare
+#combinando 3 bande spettrali: R (red), G (green), B (blue).
+
 sentinel <- c(b2, b3, b4, b8)
-plot (sentinel)
 
-#per cambiare colore
-plot (sentinel, col=inferno(100))
+# 1=b2 blue
+# 2=b3 green
+# 3=b4 red
+# 4=b8 nir
 
-plot (sentinel$sentinel.dolomites.b8) #$ in R serve per collegare vari pezzi tutti insieme
+# 3 filters and 4 bands
+im.plotRGB(sentinel, r=3, g=2, b=1) # natural colors. immagine simile a quella scattata da un aereo
+im.plotRGB(sentinel, r=4, g=3, b=2) # false colors. sostituzione banda visibile con una invisibile (NIR, infrarosso)
 
-#layer1=b2, layer2=b3, layer3=b4, layer4=b8
+
+#applicazione metodi di confronto-sovrapposizione bande visti in precedenza
+im.multiframe(1,2)
+im.plotRGB(sentinel, r=3, g=2, b=1)
+im.plotRGB(sentinel, r=4, g=3, b=2)
+
+dev.off()
+
 plot(sentinel[[4]])
-plot(sentinel[[2]])
+im.plotRGB(sentinel, r=4, g=3, b=2)
+
+
+#NIR on green
+im.plotRGB(sentinel, r=3, g=4, b=2) #false colors
+#NIR on blue
+im.plotRGB(sentinel, r=3, g=2, b=4) #false colors
+
+#Plot the four manners of RGB in a single multiframe
+im.multiframe(2,2)
+im.plotRGB(sentinel, r=3, g=2, b=1) # natural colors 
+im.plotRGB(sentinel, r=4, g=3, b=2) # false colors
+im.plotRGB(sentinel, r=3, g=4, b=2) # false colors
+im.plotRGB(sentinel, r=3, g=2, b=4) # false colors
+
+#Positioning of visible bands
+im.multiframe(1,2)
+im.plotRGB(sentinel, r=4, g=3, b=2) # false colors
+im.plotRGB(sentinel, r=4, g=2, b=3) # false colors
+
+#Semplificazione scrittura stringa (no lettere)
+im.plotRGB(sentinel, 4, 2, 3) # false colors
