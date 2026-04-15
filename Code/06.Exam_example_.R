5c83af63081047f71980a34a7f75f8ddb3a1ef0f
lucabarilaro <luca.barilaro@gmail.com>
2026-04-15 17:40:38 +0200

    Update 06.Exam_example_.R

--- Code/06.Exam_example_.R
+++ Code/06.Exam_example_.R
@@ -1,4 +1,5 @@
 library(terra) #package to manage spatial data
+library(imageRy) # package for RS didactis
 
 setwd(desktop)
 getwd()
@@ -9,3 +10,17 @@ ice <- rast ("ISS074-E-417243.jpg")
 im.multiframe(1,2)
 plot(ice[[1]])
 plot(ice[[2]])
+
+png("prime_due_band.png")
+im.multiframe(2,1)
+plot(ice[[1]])
+plot(ice[[2]])
+dev.off()
+
+# istogrammi
+png("ist.png")
+im.multiframe(3,1)
+hist(values(ice[[1]]), main="Istogramma Red", col="red")
+hist(values(ice[[2]]), main="Istogramma Green", col="green")
+hist(values(ice[[3]]), main="Istogramma Blue", col="blue")
+dev.off()
