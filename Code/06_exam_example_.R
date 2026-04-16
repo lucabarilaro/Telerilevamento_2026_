e66c91042138e1dca4339a31d61f3b913827979f
lucabarilaro <luca.barilaro@gmail.com>
2026-04-16 12:24:45 +0200

    Update 06_exam_example_.R

--- Code/06_exam_example_.R
+++ Code/06_exam_example_.R
@@ -1,17 +1,17 @@
 library(terra) #package to manage spatial data
 library(imageRy) #package for remote sensing didactis
 
-setwd() #impostare working directorty in base a dove è stata salvata immagine
+setwd("C:/Users/lucab/Documents/R") #impostare working directorty in base a dove è stata salvata immagine
 getwd()
 list.files()
 
-ice <- rast("ISS074-E-417243.jpg") #scrivere cosa fa funzione rast
+ice <- rast("ISS074-E-417243.jpg") #funzione rast del package terra serve per importorare dati raster
 
-im.multiframe(1,2)
+im.multiframe(1,2) #funzione per plottare immagini
 plot(ice[[1]])
 plot(ice[[2]])
 
-png("prime_due_band.png")
+png("prime_due_band.png") #funzione per salvare .png nella mia cartella pc
 im.multiframe(2,1)
 plot(ice[[1]])
 plot(ice[[2]])
